c0f3b3a1bcdf1d6b8f3dd2f54af461477738e3de
wolokowoh <60557333+wolokowoh@users.noreply.github.com>
2020-02-06 16:28:31 -0600

    added teleporter script

---
+++ Hellspawn_GreenGroup/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleporter : MonoBehaviour
+{
+    public UpdateUI updateUI;
+    public string MessageToDisplay;
+    public int buildIndexofSceneToLoad;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            updateUI.CutOnAndDisplayInteractionText(MessageToDisplay);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            SceneManager.LoadScene(buildIndexofSceneToLoad);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            updateUI.CutOffInteractionText();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
