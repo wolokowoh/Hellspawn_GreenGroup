a725c5b85a707cd5506790d66d8c5e783c5d1db7
wolokowoh <60557333+wolokowoh@users.noreply.github.com>
2020-02-06 22:22:47 -0600

    flesh wall decompose

---
+++ Hellspawn_GreenGroup/Assets/FleshLerp.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FleshLerp : MonoBehaviour
+{
+    public Material startMaterial;
+    public Color endColor;
+    public float time;
+    public bool rotting;
+
+    Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotting = false;
+        rend = GetComponent<Renderer>();
+        StartCoroutine(Rot());
+        
+    }
+
+    public IEnumerator Rot()
+    {
+        Debug.Log("Starting Infestation!");
+        float ElapsedTime = 0.0f;
+        float TotalTime = time;
+        while (ElapsedTime < TotalTime)
+        {
+            ElapsedTime += Time.deltaTime;
+            rend.material.color = Color.Lerp(startMaterial.color, endColor, (ElapsedTime / TotalTime));
+            yield return null;
+        }
+        Debug.Log("Ending Infestation!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
