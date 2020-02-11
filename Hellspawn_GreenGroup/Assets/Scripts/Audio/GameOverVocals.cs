131dea62e1ab1eaf436d2e8c91a479645234717d
wolokowoh <60557333+wolokowoh@users.noreply.github.com>
2020-02-10 20:07:33 -0600

    fixed an issue with clicking the gameover buttons twice

--- Hellspawn_GreenGroup/Assets/Scripts/Audio/GameOverVocals.cs
+++ Hellspawn_GreenGroup/Assets/Scripts/Audio/GameOverVocals.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class GameOverVocals : MonoBehaviour
 {
-    
+    bool coroutineinProgress;
 
     public AudioSource[] retryClips;
     public AudioSource[] deathClips;
@@ -18,40 +18,49 @@ public class GameOverVocals : MonoBehaviour
 
     public void playRetry()
     {
-        int randDeath = Random.Range(0, GetRetryClips().GetLength(0));
-        AudioSource retryClipToPlay = retryClips[randDeath];
-        retryClipToPlay.Play();
-        StartCoroutine("LoadFromCheckpoint");
+        if (!coroutineinProgress)
+        {
+            int randDeath = Random.Range(0, GetRetryClips().GetLength(0));
+            AudioSource retryClipToPlay = retryClips[randDeath];
+            retryClipToPlay.Play();
+            StartCoroutine("LoadFromCheckpoint");
+        }
+        
 
     }
 
     public IEnumerator LoadFromCheckpoint()
     {
         // if we do one save file system, just get last scene
-
+        coroutineinProgress = true;
         yield return new WaitForSeconds(7);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void playQuit()
     {
-        int randDeath = Random.Range(0, GetQuitClips().GetLength(0));
-        AudioSource quitClipToPlay = quitClips[randDeath];
-        quitClipToPlay.Play();
-        StartCoroutine("LoadMainMenu");
+        if (!coroutineinProgress)
+        {
+            int randDeath = Random.Range(0, GetQuitClips().GetLength(0));
+            AudioSource quitClipToPlay = quitClips[randDeath];
+            quitClipToPlay.Play();
+            StartCoroutine("LoadMainMenu");
+        }
+        
     }
 
     public IEnumerator LoadMainMenu()
     {
         // if we do one save file system, just get last scene
-
+        coroutineinProgress = true;
         yield return new WaitForSeconds(7);
         SceneManager.LoadScene(0);
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        coroutineinProgress = false;
+
     }
 
     // Update is called once per frame
